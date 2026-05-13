f9104267e5d1dc9312c2944a87a13dad90d9035d
p0ct4 <clexvfx@gmail.com>
2026-05-12 22:26:01 -0300

    se arreglo

--- models.py
+++ models.py
@@ -1,4 +1,13 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
+from sqlalchemy import Column, Integer, Float, String, DateTime, func
+from database import Base  
+class Lectura(Base):
+    __tablename__ = "lecturas"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    device_id = Column(String(50), nullable=False, index=True)
+    temperatura = Column(Float, nullable=False)
+    humedad = Column(Float, nullable=False)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
 
 class Vinculacion(Base):
     __tablename__ = "vinculaciones"
